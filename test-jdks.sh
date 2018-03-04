2fa7828b266f9486bfc5e61177e74af1c9135ff8
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2018-03-04 15:42:30 +0100

    Travis JDK 7, 8

--- test-jdks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JDKS=`find /usr/java -maxdepth 1 -mindepth 1 ! -type l`
-
-export JAVA_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED"
-
-for JDK in $JDKS; do
-    ./gradlew clean test -Dorg.gradle.java.home=$JDK 2>/dev/null | egrep "requires Java 7 or later to run|Starting MockK implementation|BUILD"
-done
