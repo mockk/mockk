7f849be624d0441162920f502dce9c2908d68cfd
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2018-03-05 23:32:57 +0100

    Adding test for AdditionalInterface

--- mockk/src/test/kotlin/io/mockk/junit5/ExtensionAndInitMocksTest.kt
+++ mockk/src/test/kotlin/io/mockk/junit5/ExtensionAndInitMocksTest.kt
@@ -1,13 +1,16 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package io.mockk.junit5
 
 import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.AdditionalInterface
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.impl.annotations.SpyK
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 @ExtendWith(MockKExtension::class)
 class ExtensionAndInitMocksTest {
@@ -19,6 +22,18 @@ class ExtensionAndInitMocksTest {
 
     @SpyK
     private var carSpy = Car()
+
+    @MockK
+    @AdditionalInterface(Runnable::class)
+    private lateinit var runnableCar: Car
+
+    @RelaxedMockK
+    @AdditionalInterface(Runnable::class)
+    private lateinit var runnableRelaxedCar: Car
+
+    @SpyK
+    @AdditionalInterface(Runnable::class)
+    private var runnableCarSpy = Car()
 
     @Test
     fun initMocksAfterExtension() {
@@ -37,5 +52,12 @@ class ExtensionAndInitMocksTest {
         assertSame(carSaved, car)
         assertSame(relaxedCarSaved, relaxedCar)
         assertSame(carSpySaved, carSpy)
+    }
+
+    @Test
+    fun allRunnable() {
+        assertTrue { runnableCar as Any is Runnable }
+        assertTrue { runnableRelaxedCar as Any is Runnable }
+        assertTrue { runnableCarSpy as Any is Runnable }
     }
 }
