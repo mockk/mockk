d2ed1b3d95c02cd060f92c46e4a984da2d5ade6f
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2019-02-03 23:11:13 +0100

    Fixing regression

--- agent/android/src/main/kotlin/io/mockk/proxy/android/OnjenesisInstantiator.kt
+++ agent/android/src/main/kotlin/io/mockk/proxy/android/OnjenesisInstantiator.kt
@@ -6,10 +6,11 @@ import io.mockk.proxy.MockKInstantiatior
 import org.objenesis.ObjenesisStd
 import org.objenesis.instantiator.ObjectInstantiator
 import java.lang.reflect.Modifier
+import java.lang.reflect.Proxy
 import java.util.*
 
 internal class OnjenesisInstantiator(val log: MockKAgentLogger) : MockKInstantiatior {
-    private val objenesis = ObjenesisStd(true)
+    private val objenesis = ObjenesisStd(false)
     private val instantiators = Collections.synchronizedMap(WeakHashMap<Class<*>, ObjectInstantiator<*>>())
 
     override fun <T> instance(cls: Class<T>): T {
@@ -17,21 +18,21 @@ internal class OnjenesisInstantiator(val log: MockKAgentLogger) : MockKInstantia
 
         log.trace("Creating new empty instance of $clazz")
 
-        val inst = instantiators.getOrPut(clazz) {
-            objenesis.getInstantiatorOf(clazz)
+        val inst = synchronized(instantiators) {
+            instantiators.getOrPut(clazz) {
+                objenesis.getInstantiatorOf(clazz)
+            }
         }
 
-        return clazz.cast(inst.newInstance()) as T
+        val newInstance = inst.newInstance()
+        return clazz.cast(newInstance) as T
     }
 
     @Suppress("UNCHECKED_CAST")
     private fun <T> proxyInterface(clazz: Class<T>) =
         if (Modifier.isAbstract(clazz.modifiers)) {
             if (clazz.isInterface) {
-                ProxyBuilder.forClass(Any::class.java)
-                    .parentClassLoader(clazz.classLoader)
-                    .implementing(clazz)
-                    .buildProxyClass() as Class<T>
+                Proxy.getProxyClass(clazz.classLoader, clazz) as Class<T>
             } else {
                 ProxyBuilder.forClass(clazz)
                     .parentClassLoader(clazz.classLoader)
