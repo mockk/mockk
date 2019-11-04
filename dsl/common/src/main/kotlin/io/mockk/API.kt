aca5a6dbc24029955b076921c00550805ab2663f
Mattia Tommasone <raibaz@gmail.com>
2019-11-04 23:12:42 +0100

    Changed implementation of invokeReturnsUnit to actually return Unit rather than DynamicCallLong

--- dsl/common/src/main/kotlin/io/mockk/API.kt
+++ dsl/common/src/main/kotlin/io/mockk/API.kt
@@ -1946,7 +1946,7 @@ open class MockKMatcherScope(
         invoke(name).withArguments(listOf())
 
     @Suppress("CAST_NEVER_SUCCEEDS")
-    infix fun Any.invokeReturnsUnit(name: String) = invoke(name) as Unit
+    infix fun Any.invokeReturnsUnit(name: String) = invokeNoArgs(name) as Unit
 
     infix fun Any.getProperty(name: String) =
         InternalPlatformDsl.dynamicGet(this, name)
