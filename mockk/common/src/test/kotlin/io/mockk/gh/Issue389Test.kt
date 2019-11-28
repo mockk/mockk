c135bbda5f7d664a0943b17a552e18b54f1b9bd0
Mattia Tommasone <raibaz@gmail.com>
2019-11-28 22:34:44 +0100

    Fixed issue 389 by catching AssertionErrors in the FunctionMatcher

---
+++ mockk/common/src/test/kotlin/io/mockk/gh/Issue389Test.kt
@@ -0,0 +1,38 @@
+package io.mockk.gh
+
+import io.mockk.mockk
+import io.mockk.verifyAll
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Tweet(val id: Int, val text: String)
+
+interface TweetRepository {
+
+    fun persist(tweet: Tweet)
+
+}
+
+class Issue389Test {
+
+    @Test
+    internal fun `verify multiple persists`() {
+        val repositoryMock = mockk<TweetRepository>(relaxed = true)
+
+        repositoryMock.persist(Tweet(1, "first tweet"))
+        repositoryMock.persist(Tweet(2, "second tweet"))
+
+        verifyAll {
+            repositoryMock.persist(
+                withArg {
+                    assertEquals(it.id, 1)
+                    assertEquals(it.text, "first tweet")
+                })
+            repositoryMock.persist(
+                withArg {
+                    assertEquals(it.id,2)
+                    assertEquals(it.text, "second tweet")
+                })
+        }
+    }
+}
