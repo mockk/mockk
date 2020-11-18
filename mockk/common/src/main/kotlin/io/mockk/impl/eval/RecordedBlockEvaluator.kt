c7884d5affb475e950c80991b95ab307627c12ad
Mattia Tommasone <raibaz@gmail.com>
2020-11-18 16:49:48 +0100

    Renamed the enhanceWithNPERethrow function as it is not related to NPE anymore

--- mockk/common/src/main/kotlin/io/mockk/impl/eval/RecordedBlockEvaluator.kt
+++ mockk/common/src/main/kotlin/io/mockk/impl/eval/RecordedBlockEvaluator.kt
@@ -28,7 +28,7 @@ abstract class RecordedBlockEvaluator(
                 { throw MockKException("You should specify either 'mockBlock' or 'coMockBlock'") }
             }
 
-            val blockWithRethrow = enhanceWithNPERethrow(block, callRecorderInstance::isLastCallReturnsNothing)
+            val blockWithRethrow = enhanceWithRethrow(block, callRecorderInstance::isLastCallReturnsNothing)
 
             val autoHinter = autoHinterFactory()
 
@@ -65,7 +65,7 @@ abstract class RecordedBlockEvaluator(
 
     private class NothingThrownNullPointerException : RuntimeException()
 
-    private fun <T> enhanceWithNPERethrow(
+    private fun <T> enhanceWithRethrow(
         block: () -> T,
         checkLastCallReturnsNothing: () -> Boolean
     ): () -> T =
