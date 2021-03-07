863f23a06425db378d3e1a573ce86e3fe60df645
Mattia Tommasone <raibaz@gmail.com>
2021-03-07 23:27:43 +0100

    Added another client test to check compatibility with kotlin 1.3.*

---
+++ client-tests/jvm/src/test/kotlin/io/mockk/test/AnotherClientTest.kt
@@ -0,0 +1,33 @@
+package io.mockk.test
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class MockkTest {
+
+    class Dependency1(val value1: Int)
+    class Dependency2(val value2: String)
+
+    class SystemUnderTest(
+        private val dependency1: Dependency1,
+        private val dependency2: Dependency2
+    ) {
+        fun calculate() =
+            dependency1.value1 + dependency2.value2.toInt()
+    }
+
+    @Test
+    fun calculateAddsValues() {
+        val doc1 = mockk<Dependency1>()
+        val doc2 = mockk<Dependency2>()
+
+        every { doc1.value1 } returns 5
+        every { doc2.value2 } returns "6"
+
+        val sut = SystemUnderTest(doc1, doc2)
+
+        assertEquals(11, sut.calculate())
+    }
+}
