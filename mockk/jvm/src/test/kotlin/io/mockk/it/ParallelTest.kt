42dd4a13304fd4c53d5af88f23a4f6f4b4d3fbd0
drazen04 <hangtime23@hotmail.it>
2021-05-10 23:57:09 +0200

    issue123Test reorganized

--- mockk/jvm/src/test/kotlin/io/mockk/it/ParallelTest.kt
+++ mockk/jvm/src/test/kotlin/io/mockk/it/ParallelTest.kt
@@ -1,4 +1,4 @@
-package io.mockk.gh
+package io.mockk.it
 
 import io.mockk.MockKAnnotations
 import io.mockk.every
@@ -10,7 +10,10 @@ import java.util.Random
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-class Issue123Test {
+/**
+ * Test related to github issue #123
+ */
+class ParallelTest {
     class MockCls {
         fun op(a: Int, b: Int) = a + b
     }
