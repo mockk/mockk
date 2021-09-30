e7e2030ced74e8670680f6027240b0cf4a59f14e
drazen04 <hangtime23@hotmail.it>
2021-09-30 22:57:27 +0200

    Issue323Test in NullableValueGeneratorTest

--- mockk/jvm/src/test/kotlin/io/mockk/it/NullableValueGeneratorTest.kt
+++ mockk/jvm/src/test/kotlin/io/mockk/it/NullableValueGeneratorTest.kt
@@ -3,7 +3,9 @@ package io.mockk.it
 import io.mockk.impl.JvmMockKGateway
 import io.mockk.impl.instantiation.JvmAnyValueGenerator
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.runBlocking
+import org.junit.Ignore
 import kotlin.reflect.KClass
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -43,6 +45,37 @@ class NullableValueGeneratorTest {
 
         JvmMockKGateway.anyValueGeneratorFactory = { voidInstance ->
             JvmAnyValueGenerator(voidInstance)
+        }
+    }
+
+
+    /**
+     * Test related to github issue #323
+     */
+    @Ignore("Temporarily ignored because it's failing only on travis and not anywhere else")
+    @Test
+    fun withNullableArgMatchesAndExecutesCaptureBlockWhenArgumentIsNull() {
+
+        class MockedClass {
+            fun printNullableString(s: String?) {
+                println(s)
+            }
+        }
+
+        class TestedClass(private val mockedClass: MockedClass) {
+            fun testNullString() {
+                val testString: String? = null
+                mockedClass.printNullableString(testString)
+            }
+        }
+
+        val mock = mockk<MockedClass>(relaxed = true)
+        val testedClass = TestedClass(mock)
+
+        testedClass.testNullString()
+
+        verify(exactly = 1) {
+            mock.printNullableString(withNullableArg { println(it) })
         }
     }
 }
