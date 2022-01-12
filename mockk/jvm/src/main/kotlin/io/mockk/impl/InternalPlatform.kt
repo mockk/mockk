788b1831a6152004068685a6c222899505f79d18
Mattia Tommasone <raibaz@gmail.com>
2022-01-13 00:11:33 +0100

    Temporarily remove jdk 16 and 17

--- mockk/jvm/src/main/kotlin/io/mockk/impl/InternalPlatform.kt
+++ mockk/jvm/src/main/kotlin/io/mockk/impl/InternalPlatform.kt
@@ -103,10 +103,6 @@ actual object InternalPlatform {
     actual fun <T : Any> copyFields(to: T, from: T) {
         fun copy(to: Any, from: Any, cls: Class<*>) {
             for (field in cls.declaredFields) {
-                if (Modifier.isTransient(field.modifiers)) {
-                    continue
-                }
-
                 if (Modifier.isStatic(field.modifiers)) {
                     continue
                 }
