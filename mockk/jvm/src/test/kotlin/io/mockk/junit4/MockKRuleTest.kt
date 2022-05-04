5b6fe78b5b4b4ac0f6e9a1b064ffb63044271fad
jonapoul <jpoulton@protonmail.com>
2022-05-04 09:55:52 +0100

    Test update v2

--- mockk/jvm/src/test/kotlin/io/mockk/junit4/MockKRuleTest.kt
+++ mockk/jvm/src/test/kotlin/io/mockk/junit4/MockKRuleTest.kt
@@ -8,7 +8,9 @@ import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.impl.annotations.SpyK
 import io.mockk.verify
 import org.junit.Rule
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
