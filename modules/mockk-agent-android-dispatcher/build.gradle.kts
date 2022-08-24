f7705092a91cde4bbf0b8c810a07faf069c1d852
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-08-24 19:19:34 +0900

    Fix Android Instrumentation Test failure due to `dispatcher.jar` containing unnecessary classes

--- modules/mockk-agent-android-dispatcher/build.gradle.kts
+++ modules/mockk-agent-android-dispatcher/build.gradle.kts
@@ -3,14 +3,23 @@ import buildsrc.config.asProvider
 import com.android.build.gradle.internal.tasks.DexMergingTask
 
 plugins {
-    buildsrc.convention.`android-application`
+    id("com.android.application")
 }
 
 @Suppress("UnstableApiUsage")
 android {
+    compileSdk = 32
+
     defaultConfig {
+        minSdk = 26
+        targetSdk = 32
         applicationId = "com.android.dexmaker.mockito.inline.dispatcher"
         versionCode = 1
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
