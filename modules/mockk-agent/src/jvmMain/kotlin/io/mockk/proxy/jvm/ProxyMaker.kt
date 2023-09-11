fc4b0c5d0a87d131acfb9fc4f0ff193205ad6678
Leon Rabinovich <leon@duolingo.com>
2023-09-11 08:55:40 -0400

    Remove accidentally reverted weak reference

--- modules/mockk-agent/src/jvmMain/kotlin/io/mockk/proxy/jvm/ProxyMaker.kt
+++ modules/mockk-agent/src/jvmMain/kotlin/io/mockk/proxy/jvm/ProxyMaker.kt
@@ -6,8 +6,6 @@ import io.mockk.proxy.common.transformation.InlineInstrumentation
 import io.mockk.proxy.common.transformation.TransformationRequest
 import io.mockk.proxy.common.transformation.TransformationType.SIMPLE
 import io.mockk.proxy.jvm.transformation.SubclassInstrumentation
-import java.lang.ref.SoftReference
-import java.lang.ref.WeakReference
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 
@@ -48,13 +46,10 @@ internal class ProxyMaker(
             val proxy = instantiate(actualClass, proxyClass, useDefaultConstructor, instance)
 
             handlers[proxy] = handler
-            val callbackRef = WeakReference(proxy)
             return result
                 .withValue(proxy)
                 .alsoOnCancel {
-                    callbackRef.get()?.let {
-                        handlers.remove(it)
-                    }
+                    handlers.remove(proxy)
                 }
         } catch (e: Exception) {
             result.cancel()
