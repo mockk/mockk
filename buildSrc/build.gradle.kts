ffdf8ff185ae5a6149218fa6906669f892937e37
Adam <897017+aSemy@users.noreply.github.com>
2023-09-19 14:36:20 +0200

    revert customising the Kotlin plugin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 // set the versions of Gradle plugins that the subprojects will use here
-val kotlinVersion: String = providers.gradleProperty("io_mockk_kotlin_version").getOrElse("1.9.10")
+val kotlinPluginVersion: String = "1.9.10"
 
 val androidGradle = "8.1.1"
 val kotlinxKover = "0.7.3"
@@ -13,10 +13,10 @@ val dokka = "1.9.0"
 val binaryCompatibilityValidator = "0.13.2"
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlinVersion"))
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlinPluginVersion"))
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinPluginVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinPluginVersion")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinPluginVersion")
 
     implementation("org.jetbrains.kotlinx.kover:org.jetbrains.kotlinx.kover.gradle.plugin:$kotlinxKover")
 
