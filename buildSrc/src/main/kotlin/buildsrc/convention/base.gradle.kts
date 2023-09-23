b87b4d82429747cfc67588e940b325fd8e741d0d
Simon Marquis <contact@simon-marquis.fr>
2023-09-23 12:48:10 +0200

    Enforce reproducible builds

--- buildSrc/src/main/kotlin/buildsrc/convention/base.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/base.gradle.kts
@@ -34,3 +34,9 @@ tasks.withType<Test>().configureEach {
         )
     }
 }
+
+// Enforce reproducible builds: https://docs.gradle.org/current/userguide/working_with_files.html#sec:reproducible_archives
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
