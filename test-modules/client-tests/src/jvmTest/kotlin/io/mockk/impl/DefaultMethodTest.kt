d3fff914943e9795d11b37c4033d43078448c89b
Sigurd Gerke <sigurd.gerke@onedata.de>
2025-04-11 13:13:42 +0200

    Move test to its own file

--- test-modules/client-tests/src/jvmTest/kotlin/io/mockk/impl/DefaultMethodTest.kt
+++ test-modules/client-tests/src/jvmTest/kotlin/io/mockk/impl/DefaultMethodTest.kt
@@ -3,10 +3,6 @@ package io.mockk.impl
 import io.mockk.core.ClassImplementingPackagePrivateInterfaceWithDefaultMethod
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.unmockkStatic
-import io.mockk.verify
-import org.apache.commons.lang3.RandomStringUtils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -17,16 +13,5 @@ class DefaultMethodTest {
         val obj = mockk<ClassImplementingPackagePrivateInterfaceWithDefaultMethod>()
         every { obj.foo() } returns 12
         assertEquals(12, obj.foo())
-    }
-
-    @Test
-    fun `should be able to mockk static with coverage `() {
-        mockkStatic(RandomStringUtils::class)
-        every { RandomStringUtils.secure().nextAlphanumeric(any()) } returns "x"
-
-        assertEquals(RandomStringUtils.secure().nextAlphanumeric(8), "x")
-
-        verify(exactly = 1) { RandomStringUtils.secure().nextAlphanumeric(8) }
-        unmockkStatic(RandomStringUtils::class)
     }
 }
